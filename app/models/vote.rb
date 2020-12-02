6d64ede2f57c86b175b85a30f1ab9ca8b8d3c5c9
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 11:22:08 -0600

    Validates the uniqueness of the Vote#voter attribute scoped to votable

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :votable, polymorphic: true
   belongs_to :voter, class_name: 'Teacher'
+
+  validates_uniqueness_of :voter_id, scope: %i[votable_id votable_type]
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -11,5 +11,13 @@ RSpec.describe Vote, type: :model do
     it do
       is_expected.to belong_to(:voter).class_name('Teacher')
     end
+
+    it do
+      create(:course_vote)
+
+      is_expected.to validate_uniqueness_of(:voter_id).scoped_to(
+        %i[votable_id votable_type]
+      )
+    end
   end
 end
