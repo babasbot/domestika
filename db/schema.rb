14c51b7981be40b5b5c5b664a239a60034a50691
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 11:45:48 -0600

    Adds the "courses"."votes_count" column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_165607) do
+ActiveRecord::Schema.define(version: 2020_12_02_174340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_165607) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "votes_count", default: 0, null: false
     t.index ["title"], name: "index_courses_on_title", unique: true
   end
 
