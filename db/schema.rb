7a6e89f6f0dbcb1c3adbe63e4288926f12710844
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 09:33:31 -0600

    Creates the "courses" database table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_150851) do
+ActiveRecord::Schema.define(version: 2020_12_02_152838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_courses_on_title", unique: true
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "email", null: false
