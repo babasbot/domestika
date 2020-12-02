12a4520bafec664ab019653e04c4d730de2e6098
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 12:01:02 -0600

    Adds the "proposals"."votes_count" column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_174340) do
+ActiveRecord::Schema.define(version: 2020_12_02_175941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_174340) do
     t.bigint "teacher_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "votes_count", default: 0, null: false
     t.index ["course_id", "teacher_id"], name: "index_proposals_on_course_id_and_teacher_id", unique: true
     t.index ["course_id"], name: "index_proposals_on_course_id"
     t.index ["teacher_id"], name: "index_proposals_on_teacher_id"
