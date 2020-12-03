99e065adef42c3d429ae15e5f9e30201dac7a135
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 19:25:25 -0600

    Defines the #current_teacher helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  def current_teacher
+    @current_teacher ||= Teacher.find_by(id: session[:current_teacher_id])
+  end
+  helper_method :current_teacher
 end
