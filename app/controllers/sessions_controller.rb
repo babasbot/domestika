97732d5f72fc76eeacb0b0581972a8f04531b188
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 18:14:00 -0600

    Defines the "/session/new" page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  expose :teacher
+
+  def new
+    teacher
+  end
+
+  def create
+    teacher = Teacher.find_by(email: session_params[:email])
+
+    if teacher
+      session[:current_teacher_id] = teacher.id
+
+      redirect_to root_path
+    else
+      flash[:alert] =
+        'The email and password you entered did not match our records.'
+
+      redirect_to new_session_path
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:teacher).permit(:email)
+  end
+end
---
+++ spec/requests/sessions_request_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :request do
+  describe 'GET /session/new' do
+    it do
+      get '/session/new'
+
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST /session' do
+    subject do
+      post '/session', params: { teacher: teacher_params }
+    end
+
+    context 'with an existent email' do
+      let!(:teacher) do
+        create(:teacher)
+      end
+
+      let!(:teacher_params) do
+        { email: teacher.email }
+      end
+
+      it do
+        subject
+
+        expect(response).to redirect_to(root_path)
+      end
+
+      it do
+        subject
+
+        expect(session[:current_teacher_id]).to eq teacher.id
+      end
+    end
+
+    context 'with an unexistent email' do
+      let!(:teacher_params) do
+        { email: Faker::Internet.email }
+      end
+
+      it do
+        subject
+
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it do
+        subject
+
+        expect(session[:current_teacher_id]).to be_nil
+      end
+    end
+  end
+end
