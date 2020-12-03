a003337ba7028773c3c5228ccccab7f04db3a050
Luis Alfredo Lorenzo <babasbot@gmail.com>
2020-12-02 19:29:40 -0600

    Defines the #authenticate_teacher before action callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_teacher
+
+  def authenticate_teacher
+    redirect_to new_session_path unless current_teacher
+  end
+
   def current_teacher
     @current_teacher ||= Teacher.find_by(id: session[:current_teacher_id])
   end
